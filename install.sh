cd123c5043ebf4aba69d4c3dd9fa0e512c83a0c7
UlekBR <127901575+UlekBR@users.noreply.github.com>
2023-10-18 16:54:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -x
 apt update && apt upgrade -y && apt install python3 git -y
 git clone https://github.com/UlekBR/UlekCheckUser.git
-chmod +x /root/UlekCheckuser/checkuserMenu.sh
-sudo ln -s /root/UlekCheckuser/checkuserMenu.sh /usr/local/bin/ulekCheckuser
+chmod +x /root/UlekCheckUser/checkuserMenu.sh
+ln -s /root/UlekCheckUser/checkuserMenu.sh /usr/local/bin/ulekCheckuser
 
