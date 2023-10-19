1caa0f7a2c5049a7ac48fcb9eb3cc65d64433afc
UlekBR <127901575+UlekBR@users.noreply.github.com>
2023-10-19 18:33:48 -0300

    Update

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
 set -x
+
+rm -rf /root/UlekCheckUser/
 apt update && apt upgrade -y && apt install python3 git -y
 git clone https://github.com/UlekBR/UlekCheckUser.git
 chmod +x /root/UlekCheckUser/checkuserMenu.sh
 ln -s /root/UlekCheckUser/checkuserMenu.sh /usr/local/bin/ulekCheckuser
 
 clear
-echo "Para iniciar o menu digite: $verde ulekCheckuser $reset"
+echo -e "Para iniciar o menu digite: ${verde}ulekCheckuser${reset}"
