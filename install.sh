a63d1b437fd43af28a3cdabad1a3019ff2a48e99
UlekBR <127901575+UlekBR@users.noreply.github.com>
2023-10-22 12:53:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ azul="\e[34m"
 roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
-set -x
-
 rm -rf /root/UlekCheckUser/
+rm -f /usr/local/bin/ulekCheckuser
+
+
 apt update && apt upgrade -y && apt install python3 git -y
 git clone https://github.com/UlekBR/UlekCheckUser.git
 chmod +x /root/UlekCheckUser/checkuserMenu.sh
